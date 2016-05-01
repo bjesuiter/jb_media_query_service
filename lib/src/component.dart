8dd902c47411d4586588d7cbb5f9400e65819ead
Benjamin Jesuiter <bjesuiter@gmail.com>
2016-05-01 19:22:26 +0200

    cleaned component class

--- lib/src/component.dart
+++ lib/src/component.dart
@@ -9,15 +9,10 @@ import 'package:angular/angular.dart';
 @Component(
     selector: "jb-responsive-breakpoints[active-breakpoints][breakpoints]",
     useShadowDom: true)
-class JbResponsiveBreakpoints implements ShadowRootAware, AttachAware {
-  String _currentBreakpoint;
-
+class JbResponsiveBreakpoints implements AttachAware {
   final Logger _logger = new Logger("JbResponsiveBreakpoints.Component");
 
   Element node;
-
-  int lastInnerWidth;
-  int currentInnerWidth;
 
   JbResponsiveBreakpoints(this.node) {
   }
@@ -25,41 +20,12 @@ class JbResponsiveBreakpoints implements ShadowRootAware, AttachAware {
   ///matches Media Query Strings with their labels
   Map<String, String> mediaQueries = {};
 
-  get currentBreakpoint {
-    return _currentBreakpoint;
-  }
-
-  @NgTwoWay("current-breakpoint")
-  set currentBreakpoint(String value) {
-    _currentBreakpoint = (value != null) ? value.trim() : value;
-  }
-
   ///maps min-width values in px to Names for this breakpoint
   @NgOneWay("breakpoints")
   Map<int, String> breakpoints = {};
 
   @NgTwoWay("active-breakpoints")
   List<String> activeBreakpoints = [];
-
-//  void onResize() {
-//    var newBreakpoint = _pseudoAfterElement.content;
-//
-//    //if breakpoint has changed
-//    if (newBreakpoint != currentBreakpoint) {
-//      //copy current breakpoint to last breakpoint
-//      var lastBreakpoint = currentBreakpoint;
-//
-//      _logger.finest("old Breakpoint: $lastBreakpoint");
-//      _logger.finest("new breakpoint: $newBreakpoint");
-//      currentBreakpoint = newBreakpoint;
-//    }
-//  }
-
-  @override
-  void onShadowRoot(ShadowRoot shadowRoot) {
-//    window.onResize.listen((_) => onResize());
-//    new Future(onResize);
-  }
 
   void onMediaQueryChange(MediaQueryListEvent event) {
     _logger.info("MediaQueryChange event: " + event.media);
