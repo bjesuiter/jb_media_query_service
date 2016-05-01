7c24b3e82a2db56fce81007a5d8b3055ee8de341
Benjamin Jesuiter <bjesuiter@gmail.com>
2016-05-01 19:26:34 +0200

    adjusted breakpoints

--- web/root_context.dart
+++ web/root_context.dart
@@ -4,7 +4,7 @@ import 'package:angular/angular.dart';
 @Injectable()
 class RootContext {
 
-  Map<int, String> breakpoints = {0:'small', 300:'medium', 600:'large', 900:'xlarge'};
+  Map<int, String> breakpoints = {0:'small', 300:'medium', 450:'large', 600:'xlarge'};
 
   List<String> activeBreakpoints = [];
 }
