e88b0fd7286e988c521b50479c140339b9a00a31
Benjamin Jesuiter <bjesuiter@gmail.com>
2018-01-24 00:36:18 +0100

    reformat with dartfmt

--- web/app_component.dart
+++ web/app_component.dart
@@ -2,13 +2,21 @@ import 'package:angular/angular.dart';
 import 'package:jb_responsive_breakpoints/jb_responsive_breakpoints.dart';
 import 'package:jb_responsive_breakpoints/src/test_components/consumer_component/consumer_component.dart';
 
-
 @Component(
     selector: 'app-root',
     templateUrl: 'app_component.html',
-    directives: const [CORE_DIRECTIVES, JbResponsiveBreakpoints, ConsumerComponent])
+    directives: const [
+      CORE_DIRECTIVES,
+      JbResponsiveBreakpoints,
+      ConsumerComponent
+    ])
 class AppComponent {
-  Map<int, String> breakpoints = {0:'small', 300:'medium', 450:'large', 600:'xlarge'};
+  Map<int, String> breakpoints = {
+    0: 'small',
+    300: 'medium',
+    450: 'large',
+    600: 'xlarge'
+  };
 
   List<String> activeBreakpoints = new List<String>();
 }
