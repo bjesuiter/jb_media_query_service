bc40b620cc05934f615b2997fe29a6881c001884
Benjamin Jesuiter <bjesuiter@gmail.com>
2018-01-25 11:47:25 +0100

    adds an error log when onMediaQueryChange fires but no listener is available

--- lib/src/component.dart
+++ lib/src/component.dart
@@ -1,66 +1,76 @@
 library jb_responsive_breakpoints.component;
 
-import 'dart:async';
 import 'dart:html';
 import 'package:logging/logging.dart';
 
 import 'package:angular/angular.dart';
 
-@Component(selector: "jb-responsive-breakpoints", template: "")
-class JbResponsiveBreakpoints implements AfterViewInit {
+/// mqMatch - a boolean whether the media query matches or not.
+/// This is like 'activation' and 'deactivation' of states
+typedef MediaQueryListener(bool mqMatch);
+
+@Injectable()
+class JbMediaQueryService {
   final Logger _logger = new Logger("jb_responsive_breakpoints.component");
 
-  ///matches Media Query Strings with their labels
-  Map<String, String> mediaQueries = {};
+  /// Matches Media Query Objects with their registered listeners
+  Map<String, List<MediaQueryListener>> mediaQueries = {};
 
-  ///maps min-width values in px to Names for this breakpoint
-  @Input()
-  Map<int, String> breakpoints;
+  JbMediaQueryService() {}
 
-  @Input()
-  List<String> activeBreakpoints = new List<String>();
+  /// Registers a listener for a media query breakpoint
+  registerStringQuery(MediaQueryListener listener, String mediaQuery) {
+    MediaQueryList mq = window.matchMedia(mediaQuery);
 
-  final StreamController _activeBreakpointsEmitter = new StreamController<List<String>>.broadcast();
+    _logger.fine('Original MediaQuery: ${mediaQuery}');
+    _logger.fine('Window.MatchMedia MediaQuery: ${mediaQuery}');
 
-  @Output()
-  Stream get activeBreakpointsChange => _activeBreakpointsEmitter.stream;
+    if (mediaQueries[mq.media] == null) {
+      mediaQueries[mq.media] = [];
+    }
+    mediaQueries[mq.media].add(listener);
 
-  ApplicationRef application;
+    mq.addListener(onMediaQueryChange);
 
-
-  JbResponsiveBreakpoints(this.application) {}
-
-  void onMediaQueryChange(Event event) {
-    if (event is MediaQueryListEvent) {
-      String mediaQueryLabel = mediaQueries[event.media];
-      if (event.matches) {
-        activeBreakpoints.add(mediaQueryLabel);
-        _logger.info("${mediaQueryLabel} - Label added to active breakpoints");
-      } else {
-        activeBreakpoints.remove(mediaQueryLabel);
-        _logger.info("${mediaQueryLabel} - Label removed from active breakpoints");
-      }
-      //manual change detection for whole application
-      //application.tick();
-
-      _activeBreakpointsEmitter.add(activeBreakpoints.toList());
-    } else {
-      _logger.severe('onMediaQueryChange was called with an event of different type than MediaQueryListEvent');
+    if (mq.matches) {
+      //add label for current breakpoint to activeBreakpoints list, if breakpoint matches currently
+      listener(mq.matches);
     }
   }
 
-  void ngAfterViewInit() {
-    //create media query watchers
-    breakpoints.forEach((value, label) {
-      MediaQueryList mq = window.matchMedia("(min-width: ${value}px)");
-      mediaQueries[mq.media] = label;
-      mq.addListener(onMediaQueryChange);
+  ///support some media-query features directly simplify media query string creation
+  register(Function listener, {int minWidth, int minHeight}) {
+    List<String> mediaProperties = new List<String>();
+    if (minWidth != null) {
+      mediaProperties.add('(min-width: ${minWidth}px)');
+    }
+    if (minHeight != null) {
+      mediaProperties.add('min-height: ${minHeight}px');
+    }
+    registerStringQuery(listener, mediaProperties.join(' and '));
+  }
 
-      if (mq.matches) {
-        //add label for current breakpoint to activeBreakpoints list, if breakpoint matches currently
-        activeBreakpoints.add(label);
-        _activeBreakpointsEmitter.add(activeBreakpoints.toList());
+  /// removes a listener for a specific media query
+  unregister(String mediaQuery, Function listener) {
+    mediaQueries[mediaQuery]?.remove(listener);
+
+    if (mediaQueries[mediaQuery]?.length == 0) {
+      mediaQueries.remove(mediaQuery);
+    }
+  }
+
+  void onMediaQueryChange(Event event) {
+    if (event is MediaQueryListEvent) {
+      List<MediaQueryListener> listeners = mediaQueries[event.media];
+
+      if (listeners == null) {
+        _logger.severe('Media Query Change was detected for query "${event.media}", '
+            'but no event listener could be found!');
       }
-    });
+
+      listeners?.forEach((listener) => listener(event.matches));
+    } else {
+      _logger.severe('onMediaQueryChange was called with an event of different type than MediaQueryListEvent');
+    }
   }
 }
