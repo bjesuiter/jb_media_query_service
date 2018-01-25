6880e92cf3b09a1712551e6dfea07ff32cf44b6a
Benjamin Jesuiter <bjesuiter@gmail.com>
2018-01-25 13:11:35 +0100

    fix service import path

--- example/example.dart
+++ example/example.dart
@@ -3,7 +3,7 @@ library jb_responsive_breakpoints.app_init;
 import 'package:angular/angular.dart';
 import 'package:logging/logging.dart';
 import 'package:logging_handlers/logging_handlers_shared.dart';
-import 'package:jb_responsive_breakpoints/jb_responsive_breakpoints.dart';
+import 'package:jb_media_query_service/jb_media_query_service.dart';
 
 final Logger _libLogger = new Logger("jb_responsive_breakpoints");
 
